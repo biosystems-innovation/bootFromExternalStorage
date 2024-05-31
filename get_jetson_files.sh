bb0623a67acecb722c4bba1d1f90334b39534c75
Aleix Lopez <alopez@biosystems.es>
2024-05-31 08:48:53 +0200

    R36.3.0

--- get_jetson_files.sh
+++ get_jetson_files.sh
@@ -10,7 +10,7 @@
 # First check to see if we're running on Ubuntu
 # Next, check the architecture to make sure it's not aarch64, not a Jetson
 
-JETSON_FOLDER=R36.2.0
+JETSON_FOLDER=R36.3.0
 
 
 function help_func
@@ -79,15 +79,15 @@ cd $JETSON_FOLDER
 
 # Get the 36.2.0 Tegra system
 # Get the L4T Driver Package - BSP
-wget -N https://developer.nvidia.com/downloads/embedded/l4t/r36_release_v2.0/release/jetson_linux_r36.2.0_aarch64.tbz2
+wget -N https://developer.nvidia.com/downloads/embedded/l4t/r36_release_v2.0/release/jetson_linux_r36.3.0_aarch64.tbz2
 # Get the Sample Root File System (rootfs)
-wget -N https://developer.nvidia.com/downloads/embedded/l4t/r36_release_v2.0/release/tegra_linux_sample-root-filesystem_r36.2.0_aarch64.tbz2
+wget -N https://developer.nvidia.com/downloads/embedded/l4t/r36_release_v2.0/release/tegra_linux_sample-root-filesystem_r36.3.0_aarch64.tbz2
 
 # Unpack the files, creating the Linux_for_Tegra folder
-sudo tar -xpvf jetson_linux_r36.2.0_aarch64.tbz2
+sudo tar -xpvf jetson_linux_r36.3.0_aarch64.tbz2
 
 cd Linux_for_Tegra/rootfs/
-sudo tar -xpvf ../../tegra_linux_sample-root-filesystem_r36.2.0_aarch64.tbz2
+sudo tar -xpvf ../../tegra_linux_sample-root-filesystem_r36.3.0_aarch64.tbz2
 cd ../..
 cd Linux_for_Tegra/
 
